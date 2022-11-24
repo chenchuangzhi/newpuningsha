bafd1f3ddf17778ff75714bdb9d415ea513028eb
chenchuangzhi <19927459443@163.com>
2022-11-24 22:42:26 +0800

    补上移植遗漏

--- game/update.js
+++ game/update.js
@@ -1,10 +1,13 @@
 window.noname_update={
-	version:'1.9.116.5',
+	version:'1.9.116.6',
 	update:'1.9.116.0',
 	changeLog:[
 		'补上移植遗漏',
 	],
 	files:[
+
+	    'character/daba.js',
+	    'character/liaoyuan.js',
 		'game/game.js',
 		'game/config.js',
 		'game/package.js',
