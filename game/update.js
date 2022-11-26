8ac75a6000caae86f27e90c16e8b732a1f61bc07
chenchuangzhi <19927459443@163.com>
2022-11-26 20:28:08 +0800

    更新狗卡最近的几个谋武将

--- game/update.js
+++ game/update.js
@@ -1,10 +1,11 @@
 window.noname_update={
-	version:'1.9.116.9',
+	version:'1.9.116.10',
 	update:'1.9.116.0',
 	changeLog:[
-		'更新狗卡最近的几个谋武将',
+		'更新狗卡最近的几个谋武将，修复护甲bug',
 	],
 	files:[
+	'extension/十周年UI/extension.js'
 	'image/character/bol_caocao.jpg',
 	'image/character/bol_huanggai.jpg',
 	'image/character/bol_zhouyu.jpg',
